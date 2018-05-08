29308d1d374f0bd17d36dd4081339e16c23ebe66
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2018-05-08 18:14:38 +0530

    santoni: Set androidboot.usbconfigfs in commandline for kernel 4.9
    Updated the commandline to set androidboot.usbconfigfs for kernel 4.9
    for USB enumeration to work properly. Also set required vendor properties
    for USB.
    
    Change-Id: If5b7108bc02ac11c5b70fb8393fe9a49a727fff8
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 androidboot.usbconfigfs=false loop.max_part=7
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 androidboot.usbconfigfs=true loop.max_part=7
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 loop.max_part=7 androidboot.usbconfigfs=false
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
