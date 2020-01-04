b61b25b76aa9e936e032483c83b8bb67200009d0
Atifsid <atifsiddiqui35@gmail.com>
2020-01-05 02:40:54 +0530

    Bringup lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_santoni.mk
+    $(LOCAL_DIR)/lineage_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_santoni-userdebug
+    lineage_santoni-userdebug
