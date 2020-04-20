8bd2873ca1e0d831eff1637138ae910a660ffc10
Atifsid <atifsiddiqui35@gmail.com>
2020-04-20 12:57:45 +0530

    Revert "santoni: import CAF binderized power hal"
    This reverts commit f0be331e2910b5db113e12b1bdd9245d221df108.

--- device.mk
+++ device.mk
@@ -360,9 +360,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.2-service \
-    power.qcom
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
