8dc2b9073994aebc0f0f3fa231133215220e3df6
Atifsid <atifsiddiqui35@gmail.com>
2020-04-20 12:43:57 +0530

    Revert "santoni: Add system properties for surfaceflinger"
    This reverts commit 90022d2fba990a692135c8e05a4c07c303d8e15f.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -216,10 +216,17 @@ ro.vendor.use_data_netmgrd=true \
 persist.data.netmgrd.qos.enable=true \
 persist.vendor.data.mode=concurrent
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+# Nitz
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.rild.nitz_plmn="" \
+persist.rild.nitz_long_ons_0="" \
+persist.rild.nitz_long_ons_1="" \
+persist.rild.nitz_long_ons_2="" \
+persist.rild.nitz_long_ons_3="" \
+persist.rild.nitz_short_ons_0="" \
+persist.rild.nitz_short_ons_1="" \
+persist.rild.nitz_short_ons_2="" \
+persist.rild.nitz_short_ons_3=""
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
